673e84d33e10d8b20f87472f6bd584e1a3c2ebb6
iliass <oumassateiliass@gmail.com>
2025-11-21 13:52:23 +0000

    first commit 4

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
 // Configuration de la connexion MySQL
 /*const dbConfig = {
   host: 'mysql.railway.internal',
